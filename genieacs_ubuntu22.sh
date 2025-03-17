80f66d8be22aafa8bea3f79917b827f151d518b2
wahyudien <wahyudien@users.noreply.github.com>
2025-03-17 20:07:53 +0700

    Create genieacs_ubuntu22.sh

---
+++ genieacs_ubuntu22.sh
@@ -0,0 +1,191 @@
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+local_ip=$(hostname -I | awk '{print $1}')
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}==================== Script Install GenieACS All In One. ===================${NC}"
+echo -e "${GREEN}======================== NodeJS, MongoDB, GenieACS, ========================${NC}"
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}Sebelum melanjutkan, silahkan baca terlebih dahulu. Apakah anda ingin melanjutkan? (y/n)${NC}"
+read confirmation
+if [ "$confirmation" != "y" ]; then
+    echo -e "${GREEN}Install dibatalkan. Tidak ada perubahan dalam ubuntu server anda.${NC}"
+    exit 1
+fi
+for ((i = 5; i >= 1; i--)); do
+	sleep 1
+    echo "Melanjutkan dalam $i. Tekan ctrl+c untuk membatalkan"
+done
+
+#Install NodeJS
+check_node_version() {
+    if command -v node > /dev/null 2>&1; then
+        NODE_VERSION=$(node -v | cut -d 'v' -f 2)
+        NODE_MAJOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 1)
+        NODE_MINOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 2)
+
+        if [ "$NODE_MAJOR_VERSION" -lt 12 ] || { [ "$NODE_MAJOR_VERSION" -eq 12 ] && [ "$NODE_MINOR_VERSION" -lt 13 ]; } || [ "$NODE_MAJOR_VERSION" -gt 22 ]; then
+            return 1
+        else
+            return 0
+        fi
+    else
+        return 1
+    fi
+}
+
+if ! check_node_version; then
+    echo -e "${GREEN}================== Menginstall NodeJS ==================${NC}"
+    sudo apt install curl -y
+    curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+    sudo bash nodesource_setup.sh
+    sudo apt install nodejs -y
+    sudo rm nodesource_setup.sh
+    echo -e "${GREEN}================== Sukses Menginstall NodeJS ==================${NC}"
+else
+    NODE_VERSION=$(node -v | cut -d 'v' -f 2)
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}============== NodeJS sudah terinstall versi ${NODE_VERSION}. ==============${NC}"
+    echo -e "${GREEN}========================= Lanjut install libssl1.1 ==========================${NC}"
+fi
+
+#libssl1.1
+if !  systemctl is-active --quiet libssl1.1; then
+    echo -e "${GREEN}================== Menginstall libssl1.1 ==================${NC}"
+    echo "deb http://security.ubuntu.com/ubuntu jammy-security main" | sudo tee /etc/apt/sources.list.d/jammy-security.list
+    sudo apt-get update
+    sudo apt-get install libssl1.1
+    echo -e "${GREEN}================== Sukses Menginstall libssl1.1 ==================${NC}"
+else
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}=================== libssl1.1 sudah terinstall sebelumnya. ===================${NC}"
+    echo -e "${GREEN}========================== Lanjut install MongoDB ===========================${NC}"
+fi
+    
+#MongoDB
+if !  systemctl is-active --quiet mongod; then
+    echo -e "${GREEN}================== Menginstall MongoDB ==================${NC}"
+    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+    apt-key list
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    sudo apt update
+    sudo apt install mongodb-org -y
+    sudo systemctl start mongod.service
+    sudo systemctl start mongod
+    sudo systemctl enable mongod
+    mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+    echo -e "${GREEN}================== Sukses MongoDB ==================${NC}"
+else
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}=================== MongoDB sudah terinstall sebelumnya. ===================${NC}"
+    echo -e "${GREEN}========================= Lanjut install GenieACS ==========================${NC}"
+fi
+
+#GenieACS
+if !  systemctl is-active --quiet genieacs-{cwmp,fs,ui,nbi}; then
+    echo -e "${GREEN}================== Menginstall genieACS CWMP, FS, NBI, UI ==================${NC}"
+    sudo npm install -g genieacs@1.2.13
+    sudo useradd --system --no-create-home --user-group genieacs || true
+    sudo mkdir -p /opt/genieacs
+    sudo mkdir -p /opt/genieacs/ext
+    sudo chown genieacs:genieacs /opt/genieacs/ext
+    sudo cat << EOF > /opt/genieacs/genieacs.env
+GENIEACS_CWMP_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-cwmp-access.log
+GENIEACS_NBI_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-nbi-access.log
+GENIEACS_FS_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-fs-access.log
+GENIEACS_UI_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-ui-access.log
+GENIEACS_DEBUG_FILE=/var/log/genieacs/genieacs-debug.yaml
+GENIEACS_EXT_DIR=/opt/genieacs/ext
+GENIEACS_UI_JWT_SECRET=secret
+EOF
+    node -e "console.log(\"GENIEACS_UI_JWT_SECRET=\" + require('crypto').randomBytes(128).toString('hex'))" >> /opt/genieacs/genieacs.env
+    sudo chown genieacs:genieacs /opt/genieacs/genieacs.env
+    sudo chown genieacs. /opt/genieacs -R
+    sudo chmod 600 /opt/genieacs/genieacs.env
+    sudo mkdir -p /var/log/genieacs
+    sudo chown genieacs. /var/log/genieacs
+    # create systemd unit files
+## CWMP
+    sudo cat << EOF > /etc/systemd/system/genieacs-cwmp.service
+[Unit]
+Description=GenieACS CWMP
+After=network.target
+
+[Service]
+User=genieacs
+EnvironmentFile=/opt/genieacs/genieacs.env
+ExecStart=/usr/bin/genieacs-cwmp
+
+[Install]
+WantedBy=default.target
+EOF
+
+## NBI
+    sudo cat << EOF > /etc/systemd/system/genieacs-nbi.service
+[Unit]
+Description=GenieACS NBI
+After=network.target
+ 
+[Service]
+User=genieacs
+EnvironmentFile=/opt/genieacs/genieacs.env
+ExecStart=/usr/bin/genieacs-nbi
+ 
+[Install]
+WantedBy=default.target
+EOF
+
+## FS
+    sudo cat << EOF > /etc/systemd/system/genieacs-fs.service
+[Unit]
+Description=GenieACS FS
+After=network.target
+ 
+[Service]
+User=genieacs
+EnvironmentFile=/opt/genieacs/genieacs.env
+ExecStart=/usr/bin/genieacs-fs
+ 
+[Install]
+WantedBy=default.target
+EOF
+
+## UI
+    sudo cat << EOF > /etc/systemd/system/genieacs-ui.service
+[Unit]
+Description=GenieACS UI
+After=network.target
+ 
+[Service]
+User=genieacs
+EnvironmentFile=/opt/genieacs/genieacs.env
+ExecStart=/usr/bin/genieacs-ui
+ 
+[Install]
+WantedBy=default.target
+EOF
+
+# config logrotate
+ sudo cat << EOF > /etc/logrotate.d/genieacs
+/var/log/genieacs/*.log /var/log/genieacs/*.yaml {
+    daily
+    rotate 30
+    compress
+    delaycompress
+    dateext
+}
+EOF
+    echo -e "${GREEN}========== Install APP GenieACS selesai... ==============${NC}"
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now genieacs-{cwmp,fs,ui,nbi}
+    sudo systemctl start genieacs-{cwmp,fs,ui,nbi}    
+    echo -e "${GREEN}================== Sukses genieACS CWMP, FS, NBI, UI ==================${NC}"
+else
+    echo -e "${GREEN}============================================================================${NC}"
+    echo -e "${GREEN}=================== GenieACS sudah terinstall sebelumnya. ==================${NC}"
+fi
+
+#Sukses
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
+echo -e "${GREEN}============================================================================${NC}"
